5eda9be4f952af6cf921157980ed03b65e39afa0
sviatoslav <svdatskov@gmail.com>
2026-03-09 23:14:43 -0400

    add new solution

--- tree/maximum_difference_between_node_and_ascentor.py
+++ tree/maximum_difference_between_node_and_ascentor.py
@@ -7,33 +7,21 @@ class TreeNode:
         self.right = right
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
-        def dfs(node: Optional[TreeNode]):
+        def dfs(node: Optional[TreeNode], minimum: int, maximum: int):
             if node is None:
-                return None
+                return maximum - minimum
 
-            left = dfs(node.left)
-            right = dfs(node.right)
+            minimum = min(minimum, node.val)
+            maximum = max(maximum, node.val)
 
-            if left and right:
-                min_value = left[1]
-                max_value = left[0]
-                if abs(left[0] - left[1]) < abs(right[0] - right[1]):
-                    min_value = right[1]
-                    max_value = right[0]
+            left_diff = dfs(node.left, minimum, maximum)
+            right_diff = dfs(node.right, minimum, maximum)
 
-                return max(max_value, node.val), min(min_value, node.val)
+            return max(left_diff, right_diff)
 
-
-            if left:
-                return max(left[0], node.val), min(left[1], node.val)
-
-            if right:
-                return max(right[0], node.val), min(right[1], node.val)
-
-            return node.val, node.val
-
-        result = dfs(root)
-        return abs(result[1] - result[0])
+        return dfs(root, root.val, root.val)
 
 s = Solution()
-print(s.maxAncestorDiff(TreeNode(8, TreeNode(3, TreeNode(1), TreeNode(6, TreeNode(4), TreeNode(7))), TreeNode(10, None, TreeNode(14, TreeNode(13))))))
+print(s.maxAncestorDiff(
+    TreeNode(8, TreeNode(3, TreeNode(1), TreeNode(6, TreeNode(4), TreeNode(7))),
+             TreeNode(10, None, TreeNode(14, TreeNode(13))))))
